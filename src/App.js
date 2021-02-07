aff7dc4884dbc0c4c4dc4ffd77f193fa1ef5c056
Aram <aramoffpage@gmail.com>
2021-02-07 07:29:22 +0400

    removing unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 import Test1 from './Test1';
@@ -6,22 +5,7 @@ import Test1 from './Test1';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <p>{Test1}</p>
   );
 }
 
